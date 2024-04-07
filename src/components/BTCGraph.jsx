0d21dcf78c7615f5542b7a14990624152a10ae9e
AlexTurner505 <mdabarros505@gmail.com>
2024-04-07 02:50:58 +0100

    BTC Graph

--- src/components/BTCGraph.jsx
+++ src/components/BTCGraph.jsx
@@ -1,9 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import moment from "moment";
 
 export default function BTCGraph() {
-    return (
-        <div style={{backgroundColor:'white'}}>
-            <p>BTC Graph</p>
+  const [fetchingData, setFetchingData] = useState(true);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const getData = () => {
+      const url = "https://api.coindesk.com/v1/bpi/historical/close.json";
+
+      fetch(url)
+        .then((response) => response.json())
+        .then((bitcoinData) => {
+          const sortedData = [];
+          let count = 0;
+          for (let date in bitcoinData.bpi) {
+            sortedData.push({
+              d: moment(date).format('MMM DD'),
+              p: bitcoinData.bpi[date].toLocaleString("us-EN", {
+                style: "currency",
+                currency: "USD",
+              }),
+              x: count, //previous days
+              y: bitcoinData.bpi[date], // numerical price
+            });
+            count++;
+          }
+          setData(sortedData);
+          console.log("Data: ", sortedData);
+          setFetchingData(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching data: ", error);
+        });
+    };
+
+    getData();
+  }, []);
+
+  const chartData = {
+    labels: data ? data.map(item => new Date(item.d)) : [], // Convert date strings to Date objects
+    datasets: [
+      {
+        label: "Bitcoin Price (USD)",
+        data: data ? data.map(item => item.y) : [],
+        borderColor: "rgba(75,192,192,1)",
+        backgroundColor: "rgba(75,192,192,0.2)",
+      },
+    ],
+  };
+
+  const chartOptions = {
+    scales: {
+      xAxes: [{
+        type: 'time',
+        time: {
+          unit: 'day',
+          displayFormats: {
+            day: 'MMM DD'
+          }
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          callback: function(value, index, values) {
+            return '$' + value.toLocaleString(); // Format y-axis labels as currency
+          }
+        }
+      }]
+    }
+  };
+  
+
+  return (
+    <div className="chart-container">
+      {fetchingData ? (
+        <h1>Loading</h1>
+      ) : (
+        <div>
+          <Line
+            data={chartData}
+            options={chartOptions}
+          />
         </div>
-    )
+      )}
+    </div>
+  );
+  
 }
